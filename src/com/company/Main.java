8d96d906c2b9832874c2faa30d7ebdf8f6666cc2
nurdinghejjr <rimbekovilzar@gmail.com>
2021-07-17 17:43:39 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,36 @@
+package com.company;
+
+
+import java.sql.SQLOutput;
+import java.time.Year;
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+        double[] rew = new double[15];
+        double[] rasa = {1.2, -3.4, -5.3, 2.3, 9.8, 4.2, 9.0, 4.3, 7.8, 2.8, 2.4, -6.2, 3.7, 6.8, 3, 1};
+        System.out.println(Arrays.toString(rasa));
+        System.out.println("bhjkmhvj,ccc ");
+
+
+    }else
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
